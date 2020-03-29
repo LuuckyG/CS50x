c9b16265ad4a399693d45cf81a05c8500ad1f25c
LuuckyG <40790707+LuuckyG@users.noreply.github.com>
2020-03-29 17:36:55 +0200

    Update dictionary.h

--- pset5/dictionary.h
+++ pset5/dictionary.h
@@ -3,17 +3,30 @@
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
 
+#include <ctype.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <stdio.h>
 
 // Maximum length for a word
 // (e.g., pneumonoultramicroscopicsilicovolcanoconiosis)
 #define LENGTH 45
 
+// Number of buckets in hash table
+#define TABLE_SIZE 12345
+
 // Prototypes
 bool check(const char *word);
+
 unsigned int hash(const char *word);
+
 bool load(const char *dictionary);
+
 unsigned int size(void);
+
 bool unload(void);
 
 #endif // DICTIONARY_H
+
