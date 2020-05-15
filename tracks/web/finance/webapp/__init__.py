fbd298ed977e51bfd7dc32c1bc9fd5d62031c451
Luuk <luuk.geelen@hotmail.com>
2020-05-15 18:26:04 +0200

    Added user login and registration, with authentication

--- tracks/web/finance/webapp/__init__.py
+++ tracks/web/finance/webapp/__init__.py
@@ -1,22 +1,17 @@
 from flask import Flask
 from flask_session import Session
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 from webapp.config import Config
 
-# db = SQLAlchemy()
-
-# def create_app(Config):
-#     app = Flask(__name__)
-#     app.config.from_object(Config)
-#     Session(app)
-#     db.init_app(app)
-#     return app
-
-
 app = Flask(__name__)
 app.config.from_object(Config)
 Session(app)
+
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from webapp import routes
--- tracks/web/finance/webapp/routes.py
+++ tracks/web/finance/webapp/routes.py
@@ -1,9 +1,10 @@
-from webapp import app
+from webapp import app, db, bcrypt
 from webapp.models import User
 from webapp.forms import LoginForm, RegistrationForm
 from webapp.helpers import apology, login_required, lookup, usd
 
 from flask import redirect, render_template, url_for, request, flash, session, jsonify
+from flask_login import login_user
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -47,26 +48,13 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
 
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
-
         # Query database for username
-        rows = User.query.filter_by(username=request.form.get("username"))
-
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
-
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
-
-        # Redirect user to home page
-        return redirect(url_for("index"))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for("index"))
+        else:
+            flash('Login Unsuccesful. Please check username and password', 'danger')
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
@@ -98,34 +86,11 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
 
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
-
-        # Ensure email was submitted
-        elif not request.form.get("email"):
-            return apology("must provide email", 403)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
-
-        # Query database for username
-        users = User.query.filter_by(username=request.form.get("username"))
-
-        # Ensure that username not already exists
-        if len(users) != 0:
-            return apology("This username is already taken", 403)
-        
-        # Query database for email
-        emails = User.query.filter_by(email=request.form.get("email"))
-        
-        # Ensure that email not already exists
-        if len(emails) != 0:
-            return apology("There already exists an account with this email address", 403)
-
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        # Add user to database
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
 
         # Redirect user to home page
         flash(f'Account created for {form.username.data}!', 'success')
