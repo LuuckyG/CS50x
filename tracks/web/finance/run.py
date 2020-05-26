07508e0e5962b68474165e17d4fc235b3f150524
Luuk <luuk.geelen@hotmail.com>
2020-05-26 12:45:04 +0200

    Added database migrations and shell context to app

--- tracks/web/finance/run.py
+++ tracks/web/finance/run.py
@@ -1,6 +1,16 @@
-from webapp import create_app
+from webapp import db, create_app
+from webapp.users.models import User, Role, UserRoles
+from webapp.transactions.models import Share, Transaction
+
 
 app = create_app()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Role': Role, 'UserRoles': UserRoles,
+            'Share': Share, 'Transaction': Transaction}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- tracks/web/finance/webapp/__init__.py
+++ tracks/web/finance/webapp/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_admin import Admin
 from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 from webapp.config import Config
 
-admin = Admin(template_mode='bootstrap3')
+
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+migrate = Migrate()
+admin = Admin(template_mode='bootstrap3')
+
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
@@ -19,21 +23,22 @@ def create_app(config_obj=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    db.init_app(app)
     admin.init_app(app)
     bcrypt.init_app(app)
-    db.init_app(app)
+    migrate.init_app(app, db)
     login_manager.init_app(app)
 
     from webapp.main.routes import main
-    from webapp.users.routes import users
-    from webapp.transactions.routes import transactions
-
     app.register_blueprint(main)
+
+    from webapp.users.routes import users
     app.register_blueprint(users)
+
+    from webapp.transactions.routes import transactions
     app.register_blueprint(transactions)
 
-    # with app.app_context():
-    #     db.drop_all()
-    #     db.create_all()
+    from webapp.errors.handlers import errors
+    app.register_blueprint(errors)
     
     return app
